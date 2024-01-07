50eb0844eb92f3f47da379be8a7045e895b37dd7
kavya <vijayakavya1226@gmail.com>
2024-01-07 00:00:55 -0800

    expense shell

--- jenkins-ip/main.tf
+++ jenkins-ip/main.tf
@@ -17,3 +17,16 @@ resource "aws_route53_record" "jenkins" {
   records = [data.aws_instance.instance.public_ip]
   ttl = 10
 }
+
+
+data "aws_instance" "artifactory" {
+  instance_id = "i-0e77db2d5286c3701"
+}
+
+resource "aws_route53_record" "artifactory" {
+  name    = "artifactory.kdevopsb26.online"
+  type    = "A"
+  zone_id = "Z03665552INK97CUQ6WQD"
+  records = [data.aws_instance.artifactory.public_ip]
+  ttl = 10
+}
